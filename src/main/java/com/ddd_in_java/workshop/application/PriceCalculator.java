8b21a769962874237331a0a9021c07c3ed1ed535
bas <22396258+bazsup@users.noreply.github.com>
2026-04-03 18:09:00 +0700

    session 1: no fractions scenario

--- src/main/java/com/ddd_in_java/workshop/application/PriceCalculator.java
+++ src/main/java/com/ddd_in_java/workshop/application/PriceCalculator.java
@@ -2,6 +2,6 @@ package com.ddd_in_java.workshop.application;
 
 public class PriceCalculator {
   public PriceCalculationResponse calculate(PriceCalculationRequest request) {
-    return new PriceCalculationResponse(1, "USD", request.visit_id(), request.person_id());
+    return new PriceCalculationResponse(0, "USD", request.visit_id(), request.person_id());
   }
 }
