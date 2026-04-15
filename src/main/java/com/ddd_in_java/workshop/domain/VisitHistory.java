4a2b0cc2cb2e0fc647f823e9077b1a8225c31910
bas <22396258+bazsup@users.noreply.github.com>
2026-04-16 00:47:01 +0700

    red: scaffold VisitHistory with stub methods

---
+++ src/main/java/com/ddd_in_java/workshop/domain/VisitHistory.java
@@ -0,0 +1,14 @@
+package com.ddd_in_java.workshop.domain;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+public class VisitHistory {
+
+    public void add(String personId, LocalDate date) {
+    }
+
+    public int numberOfVisitsIn(String personId, YearMonth month) {
+        return 0;
+    }
+}
