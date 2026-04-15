12fa45dcc919dec6cbed2572829375efcfa5eecc
bas <22396258+bazsup@users.noreply.github.com>
2026-04-16 00:49:34 +0700

    red: test that visits in a different month are not counted

--- src/test/java/com/ddd_in_java/workshop/domain/VisitHistoryTests.java
+++ src/test/java/com/ddd_in_java/workshop/domain/VisitHistoryTests.java
@@ -18,4 +18,12 @@ class VisitHistoryTests {
         history.add("Squirrel Gus", JULY_23);
         assertEquals(1, history.numberOfVisitsIn("Squirrel Gus", JULY));
     }
+
+    @Test
+    void visitsInDifferentMonthAreNotCounted() {
+        var history = new VisitHistory();
+        history.add("Squirrel Gus", JULY_23);
+        history.add("Squirrel Gus", LocalDate.of(2023, 7, 24));
+        assertEquals(0, history.numberOfVisitsIn("Squirrel Gus", YearMonth.of(2023, 8)));
+    }
 }
