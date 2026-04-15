47d8c1528aec3bb9ac9137ba71f7f179e9259421
bas <22396258+bazsup@users.noreply.github.com>
2026-04-16 00:47:35 +0700

    green: count all visits without filtering

--- src/main/java/com/ddd_in_java/workshop/domain/VisitHistory.java
+++ src/main/java/com/ddd_in_java/workshop/domain/VisitHistory.java
@@ -2,13 +2,18 @@ package com.ddd_in_java.workshop.domain;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VisitHistory {
 
+    private final List<Visit> visits = new ArrayList<>();
+
     public void add(String personId, LocalDate date) {
+        visits.add(new Visit(personId, date));
     }
 
     public int numberOfVisitsIn(String personId, YearMonth month) {
-        return 0;
+        return visits.size();
     }
 }
