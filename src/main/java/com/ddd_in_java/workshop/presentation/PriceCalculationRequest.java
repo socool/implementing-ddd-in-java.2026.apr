714da5b9d37a0203721c1f847057cd6973838cd9
bas <22396258+bazsup@users.noreply.github.com>
2026-04-16 01:01:02 +0700

    refactor: extract localDate() helper into PriceCalculationRequest

--- src/main/java/com/ddd_in_java/workshop/presentation/PriceCalculationRequest.java
+++ src/main/java/com/ddd_in_java/workshop/presentation/PriceCalculationRequest.java
@@ -1,8 +1,12 @@
 package com.ddd_in_java.workshop.presentation;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public record PriceCalculationRequest(String date, List<DroppedFractionRequest> dropped_fractions, String person_id,
                                       String visit_id) {
 
+    public LocalDate localDate() {
+        return LocalDate.parse(date);
+    }
 }
