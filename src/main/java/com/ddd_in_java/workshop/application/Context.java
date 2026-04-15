56a073af0758605a2e8fc68f7c39e9a0d69db7b0
bas <22396258+bazsup@users.noreply.github.com>
2026-04-16 00:52:29 +0700

    green: add visitHistory to Context

--- src/main/java/com/ddd_in_java/workshop/application/Context.java
+++ src/main/java/com/ddd_in_java/workshop/application/Context.java
@@ -1,9 +1,11 @@
 package com.ddd_in_java.workshop.application;
 
 import com.ddd_in_java.workshop.domain.ExternalVisitors;
+import com.ddd_in_java.workshop.domain.VisitHistory;
 
 public class Context {
     public final ExternalVisitors externalVisitors;
+    public final VisitHistory visitHistory = new VisitHistory();
 
     private Context(ExternalVisitors externalVisitors) {
         this.externalVisitors = externalVisitors;
