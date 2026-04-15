b70a09ea12408054a182beec1909c9d9aeaee3e8
bas <22396258+bazsup@users.noreply.github.com>
2026-04-16 00:52:04 +0700

    red: test that Context has a visitHistory

---
+++ src/test/java/com/ddd_in_java/workshop/application/ContextTests.java
@@ -0,0 +1,16 @@
+package com.ddd_in_java.workshop.application;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ContextTests {
+
+    @Test
+    void contextHasVisitHistory() {
+        var context = Context.initialize(id -> Optional.empty());
+        assertNotNull(context.visitHistory);
+    }
+}
