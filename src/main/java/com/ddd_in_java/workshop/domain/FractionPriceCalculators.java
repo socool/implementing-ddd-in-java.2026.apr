1e60662cc304a8027d3c44d1187a213b684781ab
bas <22396258+bazsup@users.noreply.github.com>
2026-04-22 04:04:30 +0700

    refactor: introduce PriceKey and FractionPriceCalculators

---
+++ src/main/java/com/ddd_in_java/workshop/domain/FractionPriceCalculators.java
@@ -0,0 +1,18 @@
+package com.ddd_in_java.workshop.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class FractionPriceCalculators {
+
+    private final Map<PriceKey, FractionPriceCalculator> calculators = new HashMap<>();
+
+    public void add(PriceKey key, FractionPriceCalculator calculator) {
+        calculators.put(key, calculator);
+    }
+
+    public Optional<FractionPriceCalculator> find(PriceKey key) {
+        return Optional.ofNullable(calculators.get(key));
+    }
+}
---
+++ src/main/java/com/ddd_in_java/workshop/domain/PriceKey.java
@@ -0,0 +1,4 @@
+package com.ddd_in_java.workshop.domain;
+
+public record PriceKey(String city, FractionType.AllowedFractionType fractionType) {
+}
